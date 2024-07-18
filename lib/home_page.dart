75c5ac5c61cb92597717e7ed9a7323c89ca93ade
Damisola Morawo <morawodamisolataiwo77@gmail.com>
2024-07-18 17:54:29 +0100

    task done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,15 +5,10 @@ class HomePage extends StatefulWidget {
 
   @override
   State<HomePage> createState() => _HomePageState();
-
-  
-
-  
 }
 
 class _HomePageState extends State<HomePage> {
-
-  final List <String> _tasks = [];
+  final List<String> _tasks = [];
   final TextEditingController _taskController = TextEditingController();
 
   void _addTask() {
@@ -22,73 +17,80 @@ class _HomePageState extends State<HomePage> {
       _taskController.clear();
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text("Task Entry App",
-        style: TextStyle(
-          color: Colors.white
-        ),),
+        title: Text(
+          "Task Entry App",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
         centerTitle: true,
-        
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            Text("Hello Damisola, update your task below",
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.blue
-            ),),
-            SizedBox(height: 40,),
+            Text(
+              "Hello Damisola, update your task below",
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 40),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextFormField(
+                controller: _taskController,
                 decoration: InputDecoration(
                   labelText: "Enter your task",
                   labelStyle: TextStyle(
-                    color: Colors.blue
+                    color: Colors.blue,
                   ),
                   border: OutlineInputBorder(
                     borderSide: BorderSide(
                       color: Colors.blue,
-                      width: 2)
+                      width: 2,
+                    ),
                   ),
-                  
-                  prefixIcon: Icon(Icons.task)
+                  prefixIcon: Icon(Icons.task),
                 ),
-                
-                      
               ),
             ),
-            SizedBox(height: 20,),
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: (){},
+              onPressed: _addTask,
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.blue,
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)
-                )
+                  borderRadius: BorderRadius.circular(5),
+                ),
               ),
-               
-             child: Text("Add",
-             style: TextStyle(
-              color: Colors.white
-             ),
-             ),
-             ),
-             Expanded(
+              child: Text(
+                "Add",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
               child: ListView.builder(
                 itemCount: _tasks.length,
-                itemBuilder: (context, index){
-                  ListTile(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+                    dense: true,
                     title: Text(_tasks[index]),
-                  )
-                }))
-
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
